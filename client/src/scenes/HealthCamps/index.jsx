bd1061889494f71b0ad3d16d38da064f376e2590
NVBS SIJANTHA <m20000706009@student.cinec.edu>
2024-03-08 15:59:53 +0530

    index.jsx changes

--- client/src/scenes/HealthCamps/index.jsx
+++ client/src/scenes/HealthCamps/index.jsx
@@ -24,7 +24,7 @@ const HealthCamps = () => {
         variant="standard"
         indicatorColor="secondary"
         textColor="secondary"
-        aria-label="Sipsal Pubuduwa tabs"
+        aria-label="Helath Camps tabs"
       >
         <Tab label="Health Camps" />
         <Tab label="Pateint Registration" />
