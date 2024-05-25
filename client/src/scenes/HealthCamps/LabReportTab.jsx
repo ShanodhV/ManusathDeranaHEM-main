db3a8d245ee162850baaa9d9845793ab230debf0
Shanodh Vithanage <shanodh@xenosys.lk>
2024-05-25 22:19:18 +0530

    Lab report tab modify

--- client/src/scenes/HealthCamps/LabReportTab.jsx
+++ client/src/scenes/HealthCamps/LabReportTab.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
+import Buttons from "components/Buttons";
+import PatientRegistrationModal from "./PatientRegistrationModal";
 
 const LabReportTab = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   return (
     <Box>
-      {/* Content for Lab Report tab */}
-      <h1>Lab Report</h1>
+      <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 2 }}>
+        <Buttons label={"Add Report data"} onClick={handleOpenModal} />
+      </Box>
+      <PatientRegistrationModal open={openModal} onClose={handleCloseModal} />
     </Box>
   );
 };
