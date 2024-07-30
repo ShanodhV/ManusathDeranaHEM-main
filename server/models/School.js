46198595681591ac2ba5b3ffc4c0da07fd15207e
Hsandeep2067 <2067hsandeep@gmail.com>
2024-07-30 09:36:45 +0530

    fixing front enf issues

--- server/models/School.js
+++ server/models/School.js
@@ -1,82 +1,53 @@
-// import mongoose from "mongoose";
-
-// const SchoolSchema = new mongoose.Schema(
-//   {
-//     schoolID: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       minlength: 2,
-//       maxlength: 100,
-//     },
-//     schoolName: {
-//       type: String,
-//       required: true,
-//     },
-//     schoolAddress: {
-//       type: String,
-//       required: true,
-//     },
-//     location: {
-//       province: {
-//         type: String,
-//         required: true,
-//       },
-//       district: {
-//         type: String,
-//         required: true,
-//       },
-//       town: {
-//         type: String,
-//         required: true,
-//       },
-//     },
-//     schoolMobileNumber: {
-//       type: String, // Assuming mobile number is stored as string
-//       required: true,
-//     },
-//     principalContact: {
-//       name: {
-//         type: String,
-//         required: true,
-//       },
-//       mobileNumber: {
-//         type: String, // Assuming mobile number is stored as string
-//         required: true,
-//       },
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// const School = mongoose.model("School", SchoolSchema);
-// export default School;
-
 import mongoose from "mongoose";
 
-const schoolSchema = new mongoose.Schema(
+const SchoolSchema = new mongoose.Schema(
   {
-  schoolId: { 
-    type: String, 
-    required: true ,
-    unique: true,
-    min:2,
-    max:100,
+    schoolId: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    schoolName: {
+      type: String,
+      required: true,
+    },
+    schoolAddress: {
+      type: String,
+      required: true,
+    },
+    location: {
+      province: {
+        type: String,
+        required: true,
+      },
+      district: {
+        type: String,
+        required: true,
+      },
+      town: {
+        type: String,
+        required: true,
+      },
+    },
+    schoolMobileNumber: {
+      type: String, // Assuming mobile number is stored as string
+      required: true,
+    },
+    principalContact: {
+      name: {
+        type: String,
+        required: true,
+      },
+      mobileNumber: {
+        type: String, // Assuming mobile number is stored as string
+        required: true,
+      },
+    },
   },
-  schoolName: { 
-    type: String, 
-    required: true 
-  },
-  schoolAddress: { type: String, required: true },
-  location: {
-    town: { type: String, required: true },
-    district: { type: String, required: true },
-    province: { type: String, required: true }
-  },
-  schoolMobileNumber: { type: String, required: true },
-  principalContact: { type: Object, required: true },
-});
+  { timestamps: true }
+);
 
-const School = mongoose.model("School", schoolSchema);
-
+const School = mongoose.model("School", SchoolSchema);
 export default School;
