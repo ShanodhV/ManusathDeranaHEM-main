d2ee6db8140119de02cbb2c0dc02e23af4776866
Hsandeep2067 <2067hsandeep@gmail.com>
2024-08-14 21:48:13 +0530

    comment2

--- client/src/scenes/SipsalPubuduwa/UpdateSchoolRegistrationModal.jsx
+++ client/src/scenes/SipsalPubuduwa/UpdateSchoolRegistrationModal.jsx
@@ -113,7 +113,10 @@ const UpdateSchoolModal = ({ openModal, closeModal, school }) => {
   const handleClickAddPerson = () => {
     setPrincipalContact([...principalContact, { pname: "", pnumber: "" }]);
   };
-  
+  // const handleClickAddPerson = () => {
+  //   setPrincipalContact([...principalContact, { pname: "", pnumber: "" }]);
+  // };
+
   // const handleClickAddPerson = () => {
   //   setPrincipalContact([...principalContact, { pname: "", pnumber: "" }]);
   // };
